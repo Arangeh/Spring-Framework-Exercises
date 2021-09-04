61ffbc4c33569d67e1a6aa80c6f51c543d291d31
Alireza Aghaei <aghayov@gmail.com>
2021-09-04 17:23:56 +0430

    Implementing CRUD operations using JDBC

--- src/main/java/com/example/servlet_exercise/HelloServlet.java
+++ src/main/java/com/example/servlet_exercise/HelloServlet.java
@@ -3,9 +3,38 @@ package com.example.servlet_exercise;
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.sql.*;
 
 public class HelloServlet extends HttpServlet {
     private String message;
+
+    public static void main(String[] args) throws SQLException {
+        String uname = "root";
+        String password = "";
+        String url = "jdbc:mysql://localhost:3306/internship";
+        String query = "select * from users";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            //TODO Aut-generated catch block
+            e.printStackTrace();
+        }
+        //System.out.println("TEST");
+        try {
+            Connection con = DriverManager.getConnection(url);
+            Statement statement = con.createStatement();
+            ResultSet result = statement.executeQuery(query);
+            while(result.next()){
+            	String UserData = "";
+            	for(int i = 1; i <= 6; i++){
+            		UserData += result.getString(i) + ':';
+				}
+            	System.out.println(UserData);
+			}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void init() {
         message = "Greetings!";
@@ -19,6 +48,8 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+        out.println("The list of the users is as follows:");
+        //code to print the list of users
     }
 
     public void destroy() {
